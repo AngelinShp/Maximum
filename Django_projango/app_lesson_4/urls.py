d366fae4b4bac58c227d6abf2f7cea0a6a80828a
AngelinShp <angelinashpspb@gmail.com>
2023-07-14 19:21:10 +0300

    Слой Template

--- Django_projango/app_lesson_4/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import dz_index
-from django.urls import path
-
-urlpatterns=[
-    path('lesson_4', dz_index)
-]
--- Django_projango/app_lesson_4/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def dz_index(request):
-    return HttpResponse('Домашка по 4 занятию')
