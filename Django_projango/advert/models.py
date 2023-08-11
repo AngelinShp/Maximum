1fcd0bdd8517a16e41964f540d3b4dbf49e23919
AngelinShp <angelinashpspb@gmail.com>
2023-08-11 18:20:53 +0300

    ДЗ (ORM и модели) + Занятие по админке

--- Django_projango/advert/models.py
+++ Django_projango/advert/models.py
@@ -21,6 +21,12 @@ class Advertisement(models.Model):
             return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
 
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html('<span style="color: green; font-weight: bold;">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
